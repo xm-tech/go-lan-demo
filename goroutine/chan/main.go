fba7da816f8368511f150c502e5ee9a28cf0ac69
xm-tech <maxm09@163.com>
2020-01-05 23:56:05 +0800

    chan struct bytes demo

--- goroutine/chan/main.go
+++ goroutine/chan/main.go
@@ -1,10 +1,11 @@
 package main
 
+import "encoding/json"
 import "fmt"
 
-type Cmd struct {
-	id   int
-	data string
+type Msg struct {
+	MsgId int    `json:"msg_id"`
+	Data  string `json:"data"`
 }
 
 func main() {
@@ -15,13 +16,30 @@ func main() {
 	resp := <-ch
 	fmt.Println(resp)
 
-	var msgChan = make(chan *Cmd, 1)
+	var msgChan = make(chan *Msg, 1)
 	go func() {
-		msgChan <- &Cmd{
-			id:   1,
-			data: "hello golang",
+		msgChan <- &Msg{
+			MsgId: 1,
+			Data:  "hello golang",
 		}
 	}()
-	msgResp := <-msgChan
-	fmt.Println(msgResp)
+	fmt.Println(<-msgChan)
+
+	var bytes, err = json.Marshal(&Msg{
+		MsgId: 2,
+		Data:  "welcome to golang",
+	})
+	if err != nil {
+		panic("json Marshal fail")
+	}
+	var bCh = make(chan []byte, 1)
+	go func() {
+		bCh <- bytes
+	}()
+	var msg Msg
+	var wrong = json.Unmarshal(<-bCh, &msg)
+	if wrong != nil {
+		panic("Unmarshal fail")
+	}
+	fmt.Printf("msg = %+v\n", msg)
 }
